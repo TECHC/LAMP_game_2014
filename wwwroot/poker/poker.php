b0c3e0638aa85c2e2e3e67e45f8c9db3b25767f9
Masahiro1118 <nm18aacttazat@gmail.com>
2014-07-26 15:02:20 +0900

    更新

--- wwwroot/poker/poker.php
+++ wwwroot/poker/poker.php
@@ -2,16 +2,16 @@
 
 session_start();
 
-require_once("define.php");
-require_once("trump.php");
-require_once("hand.php");
+require_once("Define.php");
+require_once("Trump.php");
+require_once("Hand.php");
 require_once("game_manager.php");
 require_once("ai.php");
 require_once("ai1.php");
 require_once("Status.php");
 
 $coin_check_omit = true;
-require_once('../base.inc');
+require_once('../../base.inc');
 
 // --------------------------------------------------------
 
@@ -23,12 +23,16 @@ if(isset($_SESSION['poker']) === false)
 	if($_COOKIE['coin'] < 5 )
 	{
 		// 5枚以下ならばゲームは出来ないのでゲーム選択画面へ戻す
-		header('Location: ./top.php');
+		header('Location: ../top.php');
 	}
 	else
 	{
 		// 5枚以上ならば賭ける
 		$_COOKIE['coin'] -= 5;
-		setcookie('coin',$_COOKIE['coin'],0,"");
+		setcookie('coin',$_COOKIE['coin'],0);
 	}
+
+	// 出力処理
+	$template_name = 'poker/poker.tpl';
+	require_once('../../fin.inc');
 }
